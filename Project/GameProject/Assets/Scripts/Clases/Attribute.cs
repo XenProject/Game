2415cbdf247633495cca28f4940fcd0af938af89
Xen <klimovjeka0987@gmail.com>
2016-09-05 09:17:49 +0300

    Stats System test_version

---
+++ Project/GameProject/Assets/Scripts/Clases/Attribute.cs
@@ -0,0 +1,15 @@
+﻿public class Attribute : BaseStat {
+
+	public Attribute(){
+		ExpToLevel = 50;
+		LevelModifier = 1.1f;
+	}
+
+	
+}
+
+public enum AttributeName{
+	Vitality,
+	Agility,
+	Intelligence
+}
