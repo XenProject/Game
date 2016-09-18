7508124cd54afd2f2d6b041080fe88bb2f6b51f0
Xen <klimovjeka0987@gmail.com>
2016-09-18 14:35:50 +0300

    Start LoadChar;

--- Project/GameProject/Assets/Scripts/GameMaster.cs
+++ Project/GameProject/Assets/Scripts/GameMaster.cs
@@ -4,13 +4,24 @@ using System.Collections;
 public class GameMaster : MonoBehaviour {
 	public GameObject playerCharacter;
 
+	private GameObject _pc;
+	private PlayerCharacter _pcScript;
 	// Use this for initialization
 	void Start () {
-		Instantiate(playerCharacter,new Vector3(53,10,30), Quaternion.identity);
+		_pc = Instantiate(playerCharacter,new Vector3(53,10,30), Quaternion.identity) as GameObject;
+
+		_pcScript = _pc.GetComponent<PlayerCharacter>();
 	}
 
 	// Update is called once per frame
 	void Update () {
 
 	}
+
+	public void LoadCharacter(){
+		GameSettings gsScript = GameObject.Find("__GameSettings").GetComponent<GameSettings>();
+
+		gsScript.LoadCharacterData();
+	}
+
 }
